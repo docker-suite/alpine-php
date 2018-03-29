dd516f1fe8d4a06b13db239e846f1d792ab36a6a
Hexosse <hexosse@gmail.com>
2018-03-29 21:15:29 +0200

    Switched to php.earth repository

--- 7.1/rootfs/etc/entrypoint.d/010-www-data-user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Ensure www-data user exists
-# 82 is the standard uid/gid for "www-data" in Alpine
-$(grep -q "^www-data:" /etc/group) || addgroup -g 82 -S www-data
-$(grep -q "^www-data:" /etc/passwd) || adduser -u 82 -S -D -G www-data www-data
