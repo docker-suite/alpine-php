510e0570cdd9712b19fed36ab522f51c156a6343
Hexosse <hexosse@gmail.com>
2019-06-28 12:37:53 +0200

    make sure default log folder exist

--- Dockerfiles/rootfs/etc/entrypoint.d/203-php-fpm-config.sh
+++ Dockerfiles/rootfs/etc/entrypoint.d/203-php-fpm-config.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# make sure default log folder exist
+mkdir -p /var/log/php7
+
 # Generate php-fpm config file from template
 [ -f /etc/entrypoint.d/php-fpm.cnf/setup ] && (chmod +x /etc/entrypoint.d/php-fpm.cnf/setup; /etc/entrypoint.d/php-fpm.cnf/setup)
