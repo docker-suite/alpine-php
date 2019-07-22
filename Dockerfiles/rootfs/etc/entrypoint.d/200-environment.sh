8e4ff1d049464a61e6aaa0621f12a686983ea0e4
Hexosse <hexosse@gmail.com>
2019-07-22 15:29:27 +0200

    small adjustment

---
+++ Dockerfiles/rootfs/etc/entrypoint.d/200-environment.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Make sure default log folder exist
+mkdir -p /var/log/php7
+chown www-data:www-data /var/log/php7
--- Dockerfiles/rootfs/etc/entrypoint.d/202-php-config.sh
+++ Dockerfiles/rootfs/etc/entrypoint.d/202-php-config.sh
@@ -2,3 +2,6 @@
 
 # Generate php config file from template
 [ -f /etc/entrypoint.d/php.ini.cnf/setup ] && (chmod +x /etc/entrypoint.d/php.ini.cnf/setup; /etc/entrypoint.d/php.ini.cnf/setup)
+
+# Generate php-fpm config file from template
+[ -f /etc/entrypoint.d/php-fpm.cnf/setup ] && (chmod +x /etc/entrypoint.d/php-fpm.cnf/setup; /etc/entrypoint.d/php-fpm.cnf/setup)
